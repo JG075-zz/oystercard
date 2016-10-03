96cadf4c936ebd2611e96645c06cf4990bcb304d
James Gardiner <jamesgardiner075@gmail.com>
2016-10-03 17:38:02 +0100

    Can't top_up if value exceeds limit

--- lib/oyster.rb
+++ lib/oyster.rb
@@ -1,11 +1,18 @@
 class Oyster
-  attr_accessor :balance
+  attr_reader :balance
+  attr_reader :limit
+
+  MAXIMUM_LIMIT = 90
+
   def initialize
     @balance = 0
+    @limit = MAXIMUM_LIMIT
   end
 
-  def top_up(num)
-    @balance = @balance + num
+  def top_up(amount)
+    raise "Amount exceeds limit of: #{@limit}" if amount + @balance > @limit
+    @balance = @balance + amount
     return @balance
   end
+
 end
--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -10,9 +10,17 @@ describe Oyster do
   describe "#top_up" do
     context "when given a value" do
       it "adds value to balance" do
-        subject.balance = 0
-        expect(subject.top_up(10)).to eq 10
+        before = subject.balance
+        expect(subject.top_up(10)).to eq before + 10
+      end
+    end
+
+    context "when value given exceeds the limit" do
+      it "raises an error" do
+        subject.top_up(90)
+        expect{subject.top_up(1)}.to raise_error "Amount exceeds limit of: 90"
       end
     end
   end
+
 end
