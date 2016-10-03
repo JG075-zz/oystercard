9d5e2fab000dabe91bcadee0a2bcd188d524b8f4
your name <akram.rasikh@gmail.com>
2016-10-03 16:25:34 +0100

    adding balance

--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -1,3 +1,9 @@
-describe do
-  
+require 'oyster'
+
+describe Oyster do
+  context "when initialized" do
+    it "has a default balance" do
+      expect(subject.balance).to eq 0
+    end
+  end
 end
